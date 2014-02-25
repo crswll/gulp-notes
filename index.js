4fb35f7f1acd40b8e45e45efd63f2a60c8e2d805
crswll <bill.criswell@gmail.com>
2014-02-24 19:42:39 -0500

    Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var util = require('gulp-util'),
+    fs = require('fs');
+
+const PLUGIN_NAME = 'gulp-notes';
+
+function Notes (options) {
+  var opts = options || {};
+  console.log( file.path );
+};
+
+module.exports = Notes;
